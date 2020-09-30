797cbe161710f1329509609990f15b8aa7844138
Jakob Skjerning <jakob@mentalized.net>
2020-09-30 13:16:03 +0200

    Document a bit

--- app/components/page_heading/component.rb
+++ app/components/page_heading/component.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# Page Heading
+#
+# https://tailwindui.com/components/application-ui/headings/page-headings
 class PageHeading::Component < ViewComponent::Base
   def initialize(title:, actions: [])
     super
