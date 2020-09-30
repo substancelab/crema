7aab862b229d8dfcfb756577ae18986a1677e1b5
Jakob Skjerning <jakob@mentalized.net>
2020-09-30 16:20:55 +0200

    Generalize EmailComponent

--- app/components/form/input_group/component.rb
+++ app/components/form/input_group/component.rb
@@ -9,6 +9,34 @@ module Form
         @form = form
         @object = form.object
       end
+
+      # Returns the HTML to use for the actual input field element.
+      def input_field
+        @form.send(
+          input_field_type,
+          @attribute,
+          :class => input_field_classes
+        )
+      end
+
+      # Returns an array with the CSS classes to apply to the input field
+      def input_field_classes
+        [
+          "block",
+          "duration-150",
+          "ease-in-out",
+          "form-input",
+          "transition",
+          "w-full",
+          "sm:text-sm",
+          "sm:leading-5",
+        ]
+      end
+
+      # Returns the name of the method used to generate HTML for the input field
+      def input_field_type
+        :text_field
+      end
     end
   end
 end
--- app/components/form/input_group/email_component.rb
+++ app/components/form/input_group/email_component.rb
@@ -3,6 +3,10 @@
 module Form
   module InputGroup
     class EmailComponent < Form::InputGroup::Component
+      # Returns the name of the method used to generate HTML for the input field
+      def input_field_type
+        :email_field
+      end
     end
   end
 end
