90ea5055636e9b341eed6c937706ae3aab8b02ba
Jakob Skjerning <jakob@mentalized.net>
2020-10-01 10:15:02 +0200

    Set up Stimulus

--- app/packs/scripts/application.js
+++ app/packs/scripts/application.js
@@ -11,3 +11,11 @@
 // expected
 import Rails from "rails-ujs";
 Rails.start();
+
+// Set up Stimulus
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
