a6053467f4490037971be90b3cc568183811b948
Jakob Skjerning <jakob@mentalized.net>
2020-10-08 16:08:40 +0200

    Use encrypted cookies to separate users
    https://docs.stimulusreflex.com/authentication
    
    > You can use your default Rails encrypted cookie-based sessions to
    > isolate your users into their own sessions. This works great even if
    > your application doesn't have a login system.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery :with => :exception
 
   before_action :require_authorization
+  before_action :set_action_cable_identifier
+
+  private
+
+  def set_action_cable_identifier
+    cookies.encrypted[:session_id] = session.id.to_s
+  end
 end
