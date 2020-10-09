d408e5dc692604c7d18071ac3b6045c791ac34ba
Jakob Skjerning <jakob@mentalized.net>
2020-10-09 20:12:52 +0200

    Allow user to select between valid units

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
 class Service < ApplicationRecord
+  UNITS = [
+    "per_hour",
+    "per_day",
+    "per_week",
+    "per_month",
+    "per_unit"
+  ]
+
   validates :name, :presence => true
   validates :price,
             :numericality => {:greater_than_or_equal_to => 0},
             :presence => true
-  validates :unit, :presence => true
+  validates :unit,
+            :inclusion => UNITS,
+            :presence => true
 
   def to_s
     name
--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -9,4 +9,6 @@ RSpec.describe Service, :type => :model do
   it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
 
   it { should validate_presence_of(:unit) }
+  it { should allow_values(*described_class::UNITS).for(:unit) }
+  it { should_not allow_values("", "massive").for(:unit) }
 end
