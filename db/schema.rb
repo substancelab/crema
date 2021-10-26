4d5fbfedcce300766090ab5243fe5e5b794d063f
Jakob Skjerning <jakob@mentalized.net>
2021-10-26 09:42:57 +0200

    Update schema.rb to match production

--- db/schema.rb
+++ db/schema.rb
@@ -51,23 +51,6 @@ ActiveRecord::Schema.define(version: 2021_04_16_074350) do
     t.integer "economic_product_number"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
   add_foreign_key "agreements", "customers"
   add_foreign_key "agreements", "services"
 end
