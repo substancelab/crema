9b00abfa44fc1866720c03c2393ad1513bacfdad
Jakob Skjerning <jakob@mentalized.net>
2025-04-04 13:13:28 +0200

    Render Flowbite Inputs

--- app/components/form/input_group/component.rb
+++ app/components/form/input_group/component.rb
@@ -22,25 +22,7 @@ module Form
 
       # Returns the HTML to use for the actual input field element.
       def input_field
-        @form.send(
-          input_field_type,
-          @attribute,
-          :class => input_field_classes
-        )
-      end
-
-      # Returns an array with the CSS classes to apply to the input field
-      def input_field_classes
-        [
-          "block",
-          "duration-150",
-          "ease-in-out",
-          "form-input",
-          "transition",
-          "w-full",
-          "sm:text-sm",
-          "sm:leading-5",
-        ]
+        render(::Flowbite::Input::Field.new(@form, @attribute))
       end
 
       # Returns the name of the method used to generate HTML for the input field
