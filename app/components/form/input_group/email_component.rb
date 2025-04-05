59205a894930979a983d202026ee503657774337
Jakob Skjerning <jakob@mentalized.net>
2025-04-05 17:01:39 +0200

    Render email fields

--- app/components/form/input_group/email_component.rb
+++ app/components/form/input_group/email_component.rb
@@ -3,9 +3,8 @@
 module Form
   module InputGroup
     class EmailComponent < Form::InputGroup::Component
-      # Returns the name of the method used to generate HTML for the input field
-      def input_field_type
-        :email_field
+      def input_field_class
+        ::Flowbite::Input::Email
       end
     end
   end
