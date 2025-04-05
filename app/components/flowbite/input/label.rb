2e42136518aaf1288a8b789a337c722ced050580
Jakob Skjerning <jakob@mentalized.net>
2025-04-05 21:33:24 +0200

    Add error state

--- app/components/flowbite/input/label.rb
+++ app/components/flowbite/input/label.rb
@@ -3,11 +3,20 @@
 module Flowbite
   module Input
     class Label < ViewComponent::Base
+      STATES = [
+        DEFAULT = :default,
+        ERROR = :error,
+      ].freeze
+
       def call
         @form.label(
           @attribute,
           :class => label_classes
         )
+      end
+
+      def errors?
+        @object.errors.include?(@attribute.intern)
       end
 
       def initialize(form, attribute)
@@ -19,6 +28,17 @@ module Flowbite
 
       # Returns an array with the CSS classes to apply to the label element
       def label_classes
+        case state
+        when ERROR
+          error_classes
+        else
+          default_classes
+        end
+      end
+
+      protected
+
+      def default_classes
         [
           "block",
           "mb-2",
@@ -27,6 +47,30 @@ module Flowbite
           "text-gray-900",
           "dark:text-white",
         ]
+      end
+
+      def error_classes
+        [
+          "block",
+          "mb-2",
+          "text-sm",
+          "font-medium",
+          "text-red-700",
+          "dark:text-red-500",
+        ]
+      end
+
+      # Returns the state of the label.
+      #
+      # See the STATES constant for valid values.
+      #
+      # @return [Symbol] the state of the label
+      def state
+        if errors?
+          ERROR
+        else
+          DEFAULT
+        end
       end
     end
   end
