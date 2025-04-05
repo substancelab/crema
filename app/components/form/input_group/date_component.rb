5213dba2dcd4f74924bb6b1784297c1103ae4292
Jakob Skjerning <jakob@mentalized.net>
2025-04-05 17:06:45 +0200

    Render date fields

--- app/components/form/input_group/date_component.rb
+++ app/components/form/input_group/date_component.rb
@@ -3,9 +3,8 @@
 module Form
   module InputGroup
     class DateComponent < Form::InputGroup::Component
-      # Returns the name of the method used to generate HTML for the input field
-      def input_field_type
-        :date_field
+      def input_field_class
+        ::Flowbite::Input::Date
       end
     end
   end
