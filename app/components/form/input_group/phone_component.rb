400117b56b7393dfcdcc3c222b45823177c40ce6
Jakob Skjerning <jakob@mentalized.net>
2025-04-05 17:12:54 +0200

    Render phone fields

--- app/components/form/input_group/phone_component.rb
+++ app/components/form/input_group/phone_component.rb
@@ -3,9 +3,8 @@
 module Form
   module InputGroup
     class PhoneComponent < Form::InputGroup::Component
-      # Returns the name of the method used to generate HTML for the input field
-      def input_field_type
-        :phone_field
+      def input_field_class
+        ::Flowbite::Input::Phone
       end
     end
   end
