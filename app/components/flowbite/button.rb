cbeb5d86dd075745855ba5062fa01354c1326ada
Jakob Skjerning <jakob@mentalized.net>
2025-04-07 20:54:50 +0200

    Add our first style for the button component

--- app/components/flowbite/button.rb
+++ app/components/flowbite/button.rb
@@ -8,26 +8,37 @@ module Flowbite
   # @param label [String] The text to display on the button.
   # @param type [Symbol] The type of the button. Default is :button.
   class Button < ViewComponent::Base
+    class Style
+      attr_reader :classes
+
+      delegate :fetch, :to => :classes
+
+      def initialize(classes)
+        @classes = classes
+      end
+    end
+
+    # rubocop:disable Metrics/LineLength
+    STYLES = {
+      :alternative => Style.new(
+        :default => ["py-2.5", "px-5", "me-2", "mb-2", "text-sm", "font-medium", "text-gray-900", "focus:outline-none", "bg-white", "rounded-lg", "border", "border-gray-200", "hover:bg-gray-100", "hover:text-blue-700", "focus:z-10", "focus:ring-4", "focus:ring-gray-100", "dark:focus:ring-gray-700", "dark:bg-gray-800", "dark:text-gray-400", "dark:border-gray-600", "dark:hover:text-white", "dark:hover:bg-gray-700"]
+      ),
+      :default => Style.new(
+        :default => ["text-white", "bg-blue-700", "hover:bg-blue-800", "focus:ring-4", "focus:ring-blue-300", "font-medium", "rounded-lg", "text-sm", "px-5", "py-2.5", "me-2", "mb-2", "dark:bg-blue-600", "dark:hover:bg-blue-700", "focus:outline-none", "dark:focus:ring-blue-800"]
+      ),
+    }.freeze
+
+    # rubocop:enable Metrics/LineLength
     class << self
+      def classes(state: :default, style: :default)
+        style = STYLES.fetch(style)
+        style.fetch(state)
+      end
+
       def default_classes
-        [
-          "text-white",
-          "bg-blue-700",
-          "hover:bg-blue-800",
-          "focus:ring-4",
-          "focus:ring-blue-300",
-          "font-medium",
-          "rounded-lg",
-          "text-sm",
-          "px-5",
-          "py-2.5",
-          "me-2",
-          "mb-2",
-          "dark:bg-blue-600",
-          "dark:hover:bg-blue-700",
-          "focus:outline-none",
-          "dark:focus:ring-blue-800",
-        ]
+        style = STYLES.fetch(:alternative)
+        state = :default
+        style.fetch(state)
       end
     end
 
