fe89f068a5d75c519f79a83bc93990d54d59d6c3
Jakob Skjerning <jakob@mentalized.net>
2025-04-07 21:01:02 +0200

    Add our first size option to the button component

--- app/components/flowbite/button.rb
+++ app/components/flowbite/button.rb
@@ -18,21 +18,30 @@ module Flowbite
       end
     end
 
+    SIZES = {
+      :xs => ["text-xs", "px-3", "py-2"],
+      :sm => ["text-sm", "px-3", "py-1.5"],
+      :default => ["text-sm", "px-5", "py-2.5"],
+      :lg => ["text-base", "px-5", "py-3"],
+      :xl => ["text-base", "px-6", "py-3.5"],
+    }.freeze
+
     # rubocop:disable Metrics/LineLength
     STYLES = {
       :alternative => Style.new(
-        :default => ["py-2.5", "px-5", "me-2", "mb-2", "text-sm", "font-medium", "text-gray-900", "focus:outline-none", "bg-white", "rounded-lg", "border", "border-gray-200", "hover:bg-gray-100", "hover:text-blue-700", "focus:z-10", "focus:ring-4", "focus:ring-gray-100", "dark:focus:ring-gray-700", "dark:bg-gray-800", "dark:text-gray-400", "dark:border-gray-600", "dark:hover:text-white", "dark:hover:bg-gray-700"]
+        :default => ["me-2", "mb-2", "font-medium", "text-gray-900", "focus:outline-none", "bg-white", "rounded-lg", "border", "border-gray-200", "hover:bg-gray-100", "hover:text-blue-700", "focus:z-10", "focus:ring-4", "focus:ring-gray-100", "dark:focus:ring-gray-700", "dark:bg-gray-800", "dark:text-gray-400", "dark:border-gray-600", "dark:hover:text-white", "dark:hover:bg-gray-700"]
       ),
       :default => Style.new(
-        :default => ["text-white", "bg-blue-700", "hover:bg-blue-800", "focus:ring-4", "focus:ring-blue-300", "font-medium", "rounded-lg", "text-sm", "px-5", "py-2.5", "me-2", "mb-2", "dark:bg-blue-600", "dark:hover:bg-blue-700", "focus:outline-none", "dark:focus:ring-blue-800"]
+        :default => ["text-white", "bg-blue-700", "hover:bg-blue-800", "focus:ring-4", "focus:ring-blue-300", "font-medium", "rounded-lg", "me-2", "mb-2", "dark:bg-blue-600", "dark:hover:bg-blue-700", "focus:outline-none", "dark:focus:ring-blue-800"]
       ),
     }.freeze
 
     # rubocop:enable Metrics/LineLength
     class << self
-      def classes(state: :default, style: :default)
+      def classes(size: :default, state: :default, style: :default)
         style = STYLES.fetch(style)
-        style.fetch(state)
+        classes = style.fetch(state)
+        classes + SIZES.fetch(size)
       end
 
       def default_classes
