5c3fb7a421eb50cb884c1700a074b67912bfc2e0
Jakob Skjerning <jakob@mentalized.net>
2025-04-08 09:09:17 +0200

    Convert styles from constant to class method
    This makes it possible to use inheritance to override the styles in
    subclasses, but I am curious what th eperformance/memory impact is.

--- app/components/flowbite/button/outline.rb
+++ app/components/flowbite/button/outline.rb
@@ -4,37 +4,18 @@ module Flowbite
   class Button
     class Outline < Flowbite::Button
       class << self
-        def default_classes
-          [
-            "text-blue-700",
-            "hover:text-white",
-            "border",
-            "border-blue-700",
-            "hover:bg-blue-800",
-            "focus:ring-4",
-            "focus:outline-none",
-            "focus:ring-blue-300",
-            "font-medium",
-            "rounded-lg",
-            "text-sm",
-            "px-5",
-            "py-2.5",
-            "text-center",
-            "me-2",
-            "mb-2",
-            "dark:border-blue-500",
-            "dark:text-blue-500",
-            "dark:hover:text-white",
-            "dark:hover:bg-blue-500",
-            "dark:focus:ring-blue-800",
-          ]
+        # rubocop:disable Metrics/LineLength
+        def styles
+          {
+            :default => Style.new(
+              :default => ["text-blue-700", "hover:text-white", "border", "border-blue-700", "hover:bg-blue-800", "focus:ring-4", "focus:outline-none", "focus:ring-blue-300", "font-medium", "rounded-lg", "text-center", "me-2", "mb-2", "dark:border-blue-500", "dark:text-blue-500", "dark:hover:text-white", "dark:hover:bg-blue-500", "dark:focus:ring-blue-800"]
+            ),
+            :green => Style.new(
+              :default => ["text-green-700", "hover:text-white", "border", "border-green-700", "hover:bg-green-800", "focus:ring-4", "focus:outline-none", "focus:ring-green-300", "font-medium", "rounded-lg", "text-center", "me-2", "mb-2", "dark:border-green-500", "dark:text-green-500", "dark:hover:text-white", "dark:hover:bg-green-600", "dark:focus:ring-green-800"]
+            ),
+          }
         end
-      end
-
-      private
-
-      def classes
-        self.class.default_classes
+        # rubocop:enable Metrics/LineLength
       end
     end
   end
