451f6d018f5287e75e244f2b493a0826c596f640
Jakob Skjerning <jakob@mentalized.net>
2025-04-08 19:39:01 +0200

    Extract Flowbite::Style

--- app/components/flowbite/button.rb
+++ app/components/flowbite/button.rb
@@ -8,16 +8,6 @@ module Flowbite
   # @param label [String] The text to display on the button.
   # @param type [Symbol] The type of the button. Default is :button.
   class Button < ViewComponent::Base
-    class Style
-      attr_reader :classes
-
-      delegate :fetch, :to => :classes
-
-      def initialize(classes)
-        @classes = classes
-      end
-    end
-
     SIZES = {
       :xs => ["text-xs", "px-3", "py-2"],
       :sm => ["text-sm", "px-3", "py-1.5"],
@@ -40,10 +30,10 @@ module Flowbite
       # rubocop:disable Layout/LineLength
       def styles
         {
-          :alternative => Style.new(
+          :alternative => Flowbite::Style.new(
             :default => ["me-2", "mb-2", "font-medium", "text-gray-900", "focus:outline-none", "bg-white", "rounded-lg", "border", "border-gray-200", "hover:bg-gray-100", "hover:text-blue-700", "focus:z-10", "focus:ring-4", "focus:ring-gray-100", "dark:focus:ring-gray-700", "dark:bg-gray-800", "dark:text-gray-400", "dark:border-gray-600", "dark:hover:text-white", "dark:hover:bg-gray-700"]
           ),
-          :default => Style.new(
+          :default => Flowbite::Style.new(
             :default => ["text-white", "bg-blue-700", "hover:bg-blue-800", "focus:ring-4", "focus:ring-blue-300", "font-medium", "rounded-lg", "me-2", "mb-2", "dark:bg-blue-600", "dark:hover:bg-blue-700", "focus:outline-none", "dark:focus:ring-blue-800"]
           ),
         }.freeze
